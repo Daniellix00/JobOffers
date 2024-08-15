de752f13f8fcff2fc1f0d4ae260a560655faeb71
Daniel Nagorka <danielosnag1996@gmail.com>
2024-08-15 12:38:55 +0200

    implemented_offer_module_and_tests

---
+++ src/main/java/com/joboffers/domain/offer/OfferRepository.java
@@ -0,0 +1,15 @@
+package com.joboffers.domain.offer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OfferRepository {
+
+    boolean existByOfferUrl(String offerUrl);
+    Optional<Offer>findByOfferUrl(String offerUrl);
+    List<Offer> findAll();
+    List<Offer> saveAll(List<Offer> offers);
+    Optional<Offer> findById(String id);
+    Offer save(Offer offer);
+
+}
